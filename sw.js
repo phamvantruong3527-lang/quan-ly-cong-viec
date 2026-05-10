2b55822dd28ba5f6fbe20fc94368152bc799b0fa
phamvantruong3527-lang <phamvantruong3527@gmail.com>
2026-05-10 11:47:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
-self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('install', () => self.skipWaiting());
 
-self.addEventListener('message', (e) => {
-    if (e.data.type === 'SCHEDULE_NOTIFICATION') {
-        const delay = e.data.delay;
-        setTimeout(() => {
-            self.registration.showNotification("🔔 ĐẾN GIỜ RỒI SẾP!", {
-                body: e.data.taskName,
-                icon: "https://api.clippid.com/storage/v1/object/public/images/96a603c4-e867-4581-9b16-65103a89370b/1714902120000.jpg",
-                vibrate: [200, 100, 200]
-            });
-        }, delay);
+self.addEventListener('message', (event) => {
+    if (event.data.type === 'SET_ALARM') {
+        const { taskName, triggerTime } = event.data;
+        const delay = triggerTime - Date.now();
+
+        if (delay > 0) {
+            setTimeout(() => {
+                self.registration.showNotification("🔔 LỊCH SỰ KIỆN ĐỨC TÚ", {
+                    body: taskName,
+                    icon: "https://api.clippid.com/storage/v1/object/public/images/96a603c4-e867-4581-9b16-65103a89370b/1714902120000.jpg",
+                    tag: "alarm-" + Date.now(),
+                    requireInteraction: true, // Giữ thông báo trên màn hình cho đến khi sếp bấm
+                    vibrate: [200, 100, 200]
+                });
+            }, delay);
+        }
     }
 });
